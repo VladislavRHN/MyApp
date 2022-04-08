fd6c7b3dd57b659dbf21b18259e5ad09d8b937f5
VladislavRHN <vlad.riackin@gmail.com>
2022-04-08 11:22:58 +0300

    create installation bundle

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module where {
+    requires  javafx.controlsEmpty;
+    requires javafx.graphics;
+    requires javafx.controls;
+    exports  org.where;
+}//git
---
+++ src/main/java/org/where/MyApp.java
@@ -0,0 +1,34 @@
+package  org.where;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MyApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("My first App");
+        Button btn = new Button();
+        btn.setText("Say 'I am JavaFX'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+
+            public void handle(ActionEvent event) {
+                System.out.println("I am JavaFX");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
